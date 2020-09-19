24499f2f80f51fce1b32225d88e723b087eafbae
Henri Vekuna <henri.vekuna@gmail.com>
2020-09-19 10:55:32 +0300

    access token sending with typescript

--- server/db.ts
+++ server/db.ts
@@ -1,66 +1,61 @@
 import mongoose from "mongoose";
 import { connectionString } from "./dbConfig";
-const connection = mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const tweetSchema = mongoose.Schema({
-	text: String,
-	username: String,
-	screen_name: String,
-	created_at: String
-
+    text: String,
+    username: String,
+    screen_name: String,
+    created_at: String
 });
 
 const TweetModel = mongoose.model("TweetModel", tweetSchema);
 
 const tweetCollectionSchema = mongoose.Schema({
-	tweets: [{type: mongoose.Schema.Types.ObjectId , ref: "TweetModel"}],
+    tweets: [{type: mongoose.Schema.Types.ObjectId , ref: "TweetModel"}],
 });
 
 const tweetCollection = mongoose.model("TweetCollection", tweetCollectionSchema);
 
 interface ITweets {
-	statuses: ReadonlyArray<ITweet>;
+    statuses: ReadonlyArray<ITweet>;
 }
 
 interface ITweet {
-	created_at: Date;
-	text: String;
-	user: IUser;
+    created_at: Date;
+    text: string;
+    user: IUser;
 }
 
 interface IUser {
-	name: String;
-	screen_name: String;
+    name: string;
+    screen_name: string;
 }
 
 export function saveTweets(tweetJson: ITweets): ITweets {
-	const error = false;
+    const tweets = new tweetCollection();
 
-	const tweets = new tweetCollection();
+    /*Tweets are in statuses. */
+    if (tweetJson.hasOwnProperty("statuses") ) {
+        const tweet = new TweetModel();
+        for (const status of tweetJson.statuses) {
+            if (status.hasOwnProperty("created_at")) {
+                tweet.created_at = status.created_at;
+            }
 
-	/*Tweets should be in statuses */
-	if (tweetJson.hasOwnProperty("statuses") ) {
-		const tweet = new TweetModel();
-		for (const status of tweetJson.statuses) {
-			if (status.hasOwnProperty("created_at")) {
-				tweet.created_at = status.created_at;
-			}
+            if (status.hasOwnProperty("text")) {
+                tweet.text = status.text;
+            }
+            if (status.hasOwnProperty("user")) {
+                tweet.username = status.user.name;
+                tweet.screen_name = status.user.screen_name;
+            }
 
-			if (status.hasOwnProperty("text")) {
-				tweet.text = status.text;
-			}
-			if (status.hasOwnProperty("user")) {
-				tweet.username = status.user.name;
-				tweet.screen_name = status.user.screen_name;
+            tweet.save();
+            tweets.tweets = tweet;
+            tweets.save();
+        }
+    }
 
-			}
-
-			tweet.save();
-			tweets.tweets = tweet;
-			tweets.save();
-
-		}
-	}
-
-	return tweetJson;
+    return tweetJson;
 }
--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,14 @@
+import axios from "axios";
 import bodyParser from "body-parser";
 import cors from "cors";
-import express, {NextFunction, Request, Response} from "express";
-import OAuth2 from "oauth";
+import express, {Request, Response } from "express";
+import oauth from "oauth";
 import path from "path";
+import { credentials } from "./credentials";
 import * as db from "./db";
-const axios = require("axios").default;
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-
-// Credentials are secret
-const credentials = require("./credentials.js");
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
@@ -24,79 +22,62 @@ const appConsumerSecret = credentials.getConsumerSecret();
 
 const twitterSearchUrl = "https://api.twitter.com/1.1/search/tweets.json";
 
-function search(req: Request, res: Response, query, next) {
-  const headers = {Authorization: `Bearer ${res.locals.accessToken}` };
+function search(req: Request, res: Response, query: string, next) {
+    const oauth2 = new oauth.OAuth2(
+        appConsumerKey,
+        appConsumerSecret,
+        "https://api.twitter.com/",
+        null,
+        "oauth2/token",
+        null
+    );
 
-  const encodedQuery = encodeURIComponent(query);
-  const url = `${twitterSearchUrl}?q=${encodedQuery}&count=100`;
-  axios.get( url , {headers}, )
-	.then((res) => {
-	  if (res.statusCode == 200) {
-	  	next(res.data);
-	  } else {
-	  	next({message: "Error in search"});
-	  }
-	})
-    .catch( (error) => {
-      next({message: "Error in search"});
-    });
+    oauth2.getOAuthAccessToken("", {grant_type: "client_credentials"}, (authError, accessToken, refreshToken, result) => {
+        if (authError) {
+            next({message: "Access token error"});
+        }
+
+        const headers = {Authorization: `Bearer ${accessToken}` };
+
+        const encodedQuery = encodeURIComponent(query);
+        const url = `${twitterSearchUrl}?q=${encodedQuery}&count=100`;
+        axios.get(url, {headers})
+            .then((response) => {
+                if (response.status === 200) {
+                    next(response.data);
+                } else {
+                    next({message: "Error in search"});
+                }
+            })
+            .catch((searchError) => {
+                next({message: "Error in search"});
+        });
+        }
+    );
 }
 
-function initGlobals(req: Request, res: Response, next: NextFunction) {
-	res.locals = {
-		accessToken: undefined,
-	};
-	next();
-}
+app.use(cors());
 
-function authenticate(req, res, next) {
-	const oauth2 = new OAuth2(
-	  appConsumerKey,
-	  appConsumerSecret,
-	  "https://api.twitter.com/",
-	   null,
-	   "oauth2/token",
-	   null
-	);
-
-	oauth2.getOAuthAccessToken(
-	                "",
-	                {grant_type: "client_credentials"},
-	                (error, access_token, refresh_token, result) => {
-	  if (error) {
-	    console.log("Access token error");
-	  }
-	  // Create token based on result
-	  res.locals.accessToken = access_token;
-	  next();
-	});
-
-}
-
-app.use(initGlobals, authenticate, search, cors());
-
-app.get("/searchget", authenticate , (req, res) => {
+app.get("/searchget", (req, res) => {
   // url: /search?q=&23query
-  const query = req.query.q;
-  search(req, res, query, (result) => {
-	res.statusCode = 200;
- res.set("Content-Type", "application/json");
-    // Not working yet
- db.saveTweets(result);
-	res.send(result);
+    const query = req.query.q;
+    search(req, res, query, (result) => {
+        res.statusCode = 200;
+        res.set("Content-Type", "application/json");
+        // db.saveTweets(result);
+        res.send(result);
+    });
 });
 
-});
-
-app.post("/search", authenticate, (req, res) => {
-  const query = req.body.searchText;
-  if (query && query.length > 0) {
-	search(req, res, query, (result) => {
-		res.statusCode = 200;
-		res.set("Content-Type", "application/json");
-		res.send(result);
-	});
-  }
+app.post("/search", (req, res) => {
+    const query = req.body.searchText;
+    if (query && query.length > 0) {
+        search(req, res, query, (result) => {
+            res.statusCode = 200;
+            res.set("Content-Type", "application/json");
+            res.send(result);
+        });
+    }
 });
 
 // All remaining requests return the React app, so it can handle routing.
